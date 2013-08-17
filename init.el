de1743ee015ef8b73a9581a56ee2e79b365e649e
Sam Boosalis <samboosalis@gmail.com>
2013-08-17 15:59:48 -0700

    emacs for work

--- init.el
+++ init.el
@@ -23,43 +23,6 @@
 (defun cons! (x xs) ; ~ add-to-list
   (setq xs (cons x xs)))
 
-
-;;;;;;;;;;;;;;; SHORTUCTS ;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key "\M-q" 'save-buffers-kill-terminal)
-(global-set-key "\M-`" "\C-xb")
-(global-set-key "\M-g" 'goto-line)
-;(global-set-key "\M-<up>" 'beginning-of-buffer)
-;(global-set-key "\M-<down>" 'end-of-buffer)
-(global-set-key "\M-\-" "\C-a-  -  -  -  -  -  -  -\C-o\C-a\C-n")
-(global-set-key [(control x) (control b)] 'electric-buffer-list)
-(global-set-key [C-return] 'dabbrev-expand)
-(global-set-key "\M-o" "\M-f \M-b ")
-(global-set-key (kbd "<escape>") 'save-buffer)
-(global-set-key "\C-\\" 'find-file)
-;(global-set-key (kbd "M-<escape>") 'kmacro-start-macro-or-insert-counter)
-;(global-set-key (kbd "<escape>") 'kmacro-end-call-mouse)
-(global-set-key (kbd "M-r") 'query-replace)
-;(global-set-key "\C-<right>" 'other-window) ;TODO 'windmove-* maybe?
-;(global-set-key "\C-<right>" 'BACKWARDS-other-window)
-
-(defun indent-and-next () (interactive)
-  (move-beginning-of-line 1)
-  (insert " ")
-  (next-line)
-  (move-beginning-of-line 1))
-(global-set-key "\M-j" 'indent-and-next)
-
-(defun kill-line-save () (interactive)
-  (kill-line)
-  (yank))
-(global-set-key "\M-k" 'kill-line-save)
-
-(defun force-kill-buffer ()
-  (interactive)
-  (kill-buffer (buffer-name)))
-
-(global-set-key "\C-x k" 'force-kill-buffer)
-
 ;;;;;;;;;;;;;;; SETTINGS ;;;;;;;;;;;;;;;;;;;;;;
 
 (setq inhibit-splash-screen t)
@@ -111,6 +74,31 @@
 ;;   (set-face-foreground 'region "gray60")
 ;;   (set-foreground-color "white")
 ;;   (set-cursor-color "red")
+
+;; Prevent Emacs from extending file when
+;; pressing down arrow at end of buffer.
+(setq next-line-add-newlines nil)
+;; Silently ensure newline at end of file
+;; (setq require-final-newline t)
+;; or make Emacs ask about missing newline
+(setq require-final-newline nil)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;; Smex ;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key [(meta x)] (lambda ()
+                             (interactive)
+                             (or (boundp 'smex-cache)
+                                 (smex-initialize))
+                             (global-set-key [(meta x)] 'smex)
+                             (smex)))
+
+(global-set-key [(shift meta x)] (lambda ()
+                                   (interactive)
+                                   (or (boundp 'smex-cache)
+                                       (smex-initialize))
+                                   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+                                   (smex-major-mode-commands)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;; PYTHON ;;;;;;;;;;;;;;;;;;;;;;;;
@@ -248,4 +236,80 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Remote Access ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq tramp-default-method "ssh")
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; Yelp ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'smart-tabs-mode)
+(smart-tabs-advice python-indent-line-1 python-indent)
+(defun my-yelp-python-mode-hook ()
+	  (setq tab-width 4)
+	  (setq py-indent-offset 4)
+          (let ((yelp-project-p (and buffer-file-name (or (string-match "/pg/yelp-main/" (buffer-file-name))
+                                                          (string-match "/pg/clientlibs/" (buffer-file-name))
+                                                          (string-match "/pg/submodules" (buffer-file-name))
+                                                          (and (string-match "/pg/services/" (buffer-file-name))
+                                                               (not (or (string-match "/zygote/" (buffer-file-name))
+                                                                        (string-match "/rate_tracker/" (buffer-file-name))
+                                                                        (string-match "/google/" (buffer-file-name)))))))))
+        (when yelp-project-p
+          (setq indent-tabs-mode yelp-project-p)
+          (smart-tabs-mode)))
+
+	  (setq py-indent-offset tab-width)
+	  (setq python-indent tab-width)
+	  (setq py-smart-indentation nil)
+
+	  (setq-default show-trailing-whitespace t)
+	  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+	  (message "yelp python mode, tabs is %s" indent-tabs-mode))
+
+(add-hook 'python-mode-hook 'my-yelp-python-mode-hook)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; section ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;; SHORTUCTS ;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key "\M-q" 'save-buffers-kill-terminal)
+(global-set-key "\M-`" "\C-xb")
+(global-set-key "\M-g" 'goto-line)
+;(global-set-key "\M-<up>" 'beginning-of-buffer)
+;(global-set-key "\M-<down>" 'end-of-buffer)
+(global-set-key "\M-\-" "\C-a-  -  -  -  -  -  -  -\C-o\C-a\C-n")
+(global-set-key [(control x) (control b)] 'electric-buffer-list)
+(global-set-key [C-return] 'dabbrev-expand)
+(global-set-key "\M-o" "\M-f \M-b ")
+(global-set-key (kbd "<escape>") 'save-buffer)
+(global-set-key "\C-\\" 'find-file)
+;(global-set-key (kbd "M-<escape>") 'kmacro-start-macro-or-insert-counter)
+;(global-set-key (kbd "<escape>") 'kmacro-end-call-mouse)
+(global-set-key (kbd "M-r") 'query-replace)
+;(global-set-key "\C-<right>" 'other-window) ;TODO 'windmove-* maybe?
+;(global-set-key "\C-<right>" 'BACKWARDS-other-window)
+
+(defun indent-and-next () (interactive)
+  (move-beginning-of-line 1)
+  (insert " ")
+  (next-line)
+  (move-beginning-of-line 1))
+(global-set-key "\M-j" 'indent-and-next)
+
+(defun kill-line-save () (interactive)
+  (kill-line)
+  (yank))
+(global-set-key "\M-k" 'kill-line-save)
+
+(defun force-kill-buffer ()
+  (interactive)
+  (kill-buffer (buffer-name)))
+
+(global-set-key "\C-x k" 'force-kill-buffer)
+
+
