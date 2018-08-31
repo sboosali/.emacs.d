55710794c251ad175942f14de8836f921c42d856
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-30 22:15:57 -0700

    [done] simple v1 natlink grammar major-mode

--- sboo/configuration/20-my-packages/dictation/natlink-grammar.el
+++ sboo/configuration/20-my-packages/dictation/natlink-grammar.el
@@ -1,5 +1,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Simple `major-mode' for editing `Dragon-NaturallySpeaking' (`NatLink') gramamrs.
+;; Simple `major-mode' for editing `Dragon-NaturallySpeaking' (`NatLink') grammars.
 ;;
 ;; The format is (E)BNF, with:
 ;;
@@ -12,29 +12,46 @@
 ;; This format is a human-readable represenation of `Microsoft's `SAPI'
 ;; (the "Speech API"), which is a binary format.
 ;;
+;; Its canonical parser is `gramparser.py`, at 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-generic-mode 'natlink-grammar-mode
 
-  ()                                           ;; comment char: inapplicable because # must be at start of line
-  nil                                          ;; keywords
+  ()                                              ;; comment char: inapplicable because # must be at start of line
+
+  '("imported" "exported")                        ;; keywords
+
   '(
-    ("^#.*" . 'font-lock-comment-face)         ;; comments at start of line
-    ("^<.*?>" . 'font-lock-function-name-face) ;; LHS nonterminals
-    ("<.*?>" . 'font-lock-builtin-face)        ;; other nonterminals
-    ("::=" . 'font-lock-const-face)            ;; "goes-to" symbol
-    ("\|" . 'font-lock-warning-face)           ;; "OR" symbol
-    ("\{:\\|:\}" . 'font-lock-keyword-face)    ;; special pybnf delimiters
+    ("^#.*"      . 'font-lock-comment-face)       ;; comments at start of line
+    ("^<.*?>"    . 'font-lock-function-name-face) ;; LHS nonterminals
+    ("<.*?>"     . 'font-lock-variable-name-face) ;; other nonterminals
+    ("{.*?}"     . 'font-lock-variable-name-face) ;;
+    ("<dgn.*?>"  . 'font-lock-builtin-face)       ;; 
+    ("="         . 'font-lock-constant-face)      ;; "goes-to" symbol
+    (";"         . 'font-lock-constant-face)      ;; statement delimiter
+    ("\|"        . 'font-lock-keyword-face)       ;; "OR" symbol
+    ("\+"        . 'font-lock-keyword-face)       ;; 
+    ("\?"        . 'font-lock-keyword-face)       ;; 
+    ("\["        . 'font-lock-keyword-face)       ;; 
+    ("\]"        . 'font-lock-keyword-face)       ;; 
    )
-  '("\\.sapi\\'")               ;; filename suffixes
-  nil                                          ;; extra function hooks
+
+  '("\\.sapi\\'")                                 ;; filename suffixes
+
+  nil                                             ;; extra function hooks
   
-  "Major mode for BNF highlighting.")
+  "Major mode for highlighting a NatLink/SAPI grammar.")
+        
+        ;; token can be '=', '|', '+', ';', '(', ')', '[', ']' (with value None)
+        ;; or 'list' (value without {})
+        ;; or 'rule' (value wihtout <>)
+        ;; or 'sqword', 'dqword', 'word'  (a word, in single quotes, double quotes or unquoted)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-generic-mode 'bnf-mode
-  
+
   ()                                               ;; comment char:
                                                    ;; inapplicable because # must be at start of line
   nil                                              ;; keywords
@@ -56,6 +73,28 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; NOTES
+
+;; `font-lock-...-face':
+;;
+;; C-h v font-lock-.+-face'
+;;
+;;   font-lock-doc-face
+;;   font-lock-type-face
+;;   font-lock-string-face
+;;   font-lock-comment-face
+;;   font-lock-warning-face
+;;   font-lock-keyword-face
+;;   font-lock-builtin-face
+;;   font-lock-constant-face
+;;   font-lock-reference-face
+;;   font-lock-preprocessor-face
+;;   font-lock-negation-char-face
+;;   font-lock-function-name-face
+;;   font-lock-variable-name-face
+;;   font-lock-comment-delimiter-face
+;;
+;; 
+
 
 ;; See:
 ;;     - https://stackoverflow.com/questions/1800199/is-there-a-bnf-mode-for-emacs
