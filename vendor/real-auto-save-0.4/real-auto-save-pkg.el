10b40ac8c50bb48f3b79a2d7429d71ed4fe4fd6b
Spiros Boosalis <SamBoosalis@gmail.com>
2018-09-25 22:38:08 -0700

    [wip] cleanup vendored stuff

--- vendor/real-auto-save-0.4/real-auto-save-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "real-auto-save" "0.4" "enable real auto save" 'nil :commit "879144ca7e9bfa09a4fb57d5fe92a80250311f1e" :authors '(("Chaoji Li <lichaoji AT gmail DOT com>") ("Anand Reddy Pandikunta <anand21nanda AT gmail DOT com>")) :maintainer '("Chaoji Li <lichaoji AT gmail DOT com>"))
