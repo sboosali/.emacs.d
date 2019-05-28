286e89f7ecf8171bdf9b91b5a3355df26c9a2210
Sam Boosalis <SamBoosalis@gmail.com>
2019-05-28 03:28:46 -0700

    add « sboo-prose »

---
+++ sboo/sboo-prose.el
@@ -0,0 +1,78 @@
+;;; sboo-prose.el --- -*- coding: utf-8; lexical-binding: t -*-
+
+;; Copyright © 2019 Spiros Boosalis
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "25") seq pcase)
+;; Author:  Spiros Boosalis <samboosalis@gmail.com>
+;; Homepage: https://github.com/sboosali/.emacs.d
+;; Keywords: local
+;; Created: 25 May 2019
+;; License: GPL-3.0-or-later
+
+;; This file is not part of GNU Emacs.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Personal configuration for writing prose.
+;; 
+;; 
+
+;;; Code:
+
+;;----------------------------------------------;;
+;; Imports -------------------------------------;;
+;;----------------------------------------------;;
+
+;; builtins:
+
+(eval-when-compile
+  (require 'rx)
+  (require 'pcase))
+
+;;----------------------------------------------;;
+
+(progn
+  (require 'seq)
+  (require 'cl-lib))
+
+;;----------------------------------------------;;
+;; Mode ----------------------------------------;;
+;;----------------------------------------------;;
+
+(define-derived-mode sboo-prose-mode text-mode "Prose[sboo]"
+
+  (progn
+
+    (visual-line-mode +1)
+
+    #'sboo-prose-mode))
+
+;;----------------------------------------------;;
+;; Notes ---------------------------------------;;
+;;----------------------------------------------;;
+
+;; 
+;;
+;;
+
+;;----------------------------------------------;;
+;; EOF -----------------------------------------;;
+;;----------------------------------------------;;
+
+(provide 'sboo-prose)
+
+;;; sboo-prose.el ends here
